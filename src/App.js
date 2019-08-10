0085f2d256d3cb7dabeea2c905a7c366a8aaf93f
kenpeter <kenpeter@kenpeter.com>
2019-08-10 16:04:09 +1000

    IE blank page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
 
-const meta = 'data:application/pdf;base64,';
-
-const pdfData = meta +  
+/*
+// 1. works
+// 2. IE blob, download file
+// 3. not-ie, new win iframe
+function pdf() {
+  const data = '' +  
     'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog' +
     'IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv' +
     'TWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0K' +
@@ -18,10 +21,71 @@ const pdfData = meta +
     'MDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9v' +
     'dCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G';
 
+  if (data === "" || data === undefined) {
+    alert("Falied to open PDF.");
+} else { 
+    // ie, it may work
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      let byteCharacters = atob(data);
+      let byteNumbers = new Array(byteCharacters.length);
+      for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      let byteArray = new Uint8Array(byteNumbers);
+      let blob = new Blob([byteArray], {
+          type: 'application/pdf'
+      });
+
+      let fileName = 'testme';
+      window.navigator.msSaveOrOpenBlob(blob, fileName);
+    } else { 
+        // not ie
+        let meta = 'data:application/pdf;base64,';
+        let iframe = "<iframe width='100%' height='100%' src='" + meta + data + "'></iframe>"
+        let x = window.open();
+        x.document.open();
+        x.document.write(iframe);
+        x.document.close();
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <Iframe src={pdfData}
+      <button onClick={() => {
+        pdf();
+      }}>
+        click
+      </button>
+    </div>
+  );
+}
+*/
+
+// 1. react-iframe
+// 2. test on IE 15 edge windows 10, blank page
+function App() {
+  const meta = 'data:application/pdf;base64,';
+
+  const data = meta +  
+    'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog' +
+    'IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv' +
+    'TWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0K' +
+    'Pj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAg' +
+    'L1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+' +
+    'PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9u' +
+    'dAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2Jq' +
+    'Cgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJU' +
+    'CjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVu' +
+    'ZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4g' +
+    'CjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAw' +
+    'MDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9v' +
+    'dCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G';
+
+  return (
+    <div className="App">
+      <Iframe src={data}
         width="450px"
         height="450px"
         id="myId"
@@ -31,5 +95,8 @@ function App() {
     </div>
   );
 }
+
+
+
 
 export default App;
